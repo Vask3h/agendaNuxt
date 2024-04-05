2bffd2647a4364e5e6d5f8ad7fe31d04cb256980
david <ramosdavu2006@gmail.com>
2024-04-05 17:53:25 +0200

    my part from the web

---
+++ components/Add/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex justify-evenly bg-orange-700 p-2 ">
+        <NuxtImg src="/topgroupexpress.svg"/>
+        <input placeholder="Buscar contacto" type="text" class=" placeholder-white border-orange-500 rounded-xl p-2 bg-orange-500 text-white">
+        <button class="border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500 p-2 text-white">Añadir contacto</button>
+        <button class="border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500 p-2 text-white">Eliminar contacto</button>
+        <button class="border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500  text-white">Editar contacto</button>
+    </div>
+</template>
