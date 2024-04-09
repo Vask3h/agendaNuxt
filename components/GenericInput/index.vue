ee34f504a4a1b111b0123a3805acaced300f9b39
david <ramosdavu2006@gmail.com>
2024-04-09 12:42:38 +0200

    creacion de inputs

---
+++ components/GenericInput/index.vue
@@ -0,0 +1,27 @@
+<template >
+    <div class="flex items-center flex-col justify-center gap-y-1 pt-10">
+        <label class="text-white">{{ textLabel }}</label>
+        <input :placeholder="textPlaceholder" :type="typeInput" class="text-white bg-gray-700 placeholder-gray border border-solid outline-none focus-within:border-blue-500 border-gray-600 rounded-lg p-2" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'GenericInput',
+        props : {
+            textLabel:{
+                type: String,
+                default: "",
+            },
+            typeInput:{
+                type: String,
+                default:"text",
+            },
+            textPlaceholder:{
+                type: String,
+                default:"Introduce información",
+            }
+        }
+    }
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,40 @@
     <GenericActions/>
     <GenericContact/>
     <GenericNameIcon/>
+    <div class="dark:bg-gray-900">
+        <GenericInput 
+            textLabel="Nombre"
+            textPlaceholder="Inserte nombre"
+        />
+        <GenericInput 
+            textLabel="Apellido"
+            textPlaceholder="Inserte apellido"
+        /> 
+        <GenericInput 
+            textLabel="Telefono"
+            typeInput="number"
+            textPlaceholder="Inserte numero de telefono"
+        />
+         <GenericInput 
+            textLabel="Correo"
+            textPlaceholder="Inserte@correo"
+        />
+    </div>
+    
 </template>
 
 <script>
-
-    export default {
-        data: () => ({})
+    export default{
+        data:() => ({
+            text:"Prueba"
+        }),
+        computed: {
+            onTextComputed(){
+                return "Pepito"
+            }
+        }
+        
+        
     }
+    
 </script>
