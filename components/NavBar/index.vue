1a57d08bb304164ab808106a6e6500d7328d7eef
david <ramosdavu2006@gmail.com>
2024-04-09 12:42:38 +0200

    creacion de inputs

--- components/NavBar/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex justify-evenly bg-orange-700 p-2 ">
-        <NuxtImg src="/topgroupexpress.svg"/>
-        <input placeholder="Buscar contacto" type="text" class=" placeholder-white border-orange-500 rounded-xl p-2 bg-orange-500 text-white">
-        <button class="hover:bg-yellow-500 border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500 p-2 text-white" @click="" >Añadir contacto</button>
-        <button class="hover:bg-yellow-500 border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500 p-2 text-white" >Eliminar contacto</button>
-        <button class="hover:bg-yellow-500 border-solid border-orange-500 border-2 rounded-xl p-2 bg-orange-500  text-white" >Editar contacto</button>
-    </div>
-</template>
