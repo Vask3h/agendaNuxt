b3a651eb98af583267cab2abbecb97cb0ce383dd
Daniel <danielvazquez245.dv@gmail.com>
2024-04-10 15:00:02 +0200

    index.vue actualizado

--- components/Generic/contactItem.vue
+++ components/Generic/contactItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bg-gray-900 flex flex-row justify-between p-2 text-center items-center text-white text-lg max-w-7xl m-auto border-t-2 border-gray-700 ">
+  <div class="bg-gray-900 p-4 grid grid-cols-[100px_1fr_1fr_1fr_100px] text-white items-center overflow-hidden text-ellipsis border-t-2 border-gray-700 ">
     <GenericNameIcon/>
-    <p> Nombre Apellido </p>
-    <p> 123456789</p>
-    <p> example@gmail.com</p>
+    <p> {{ name}} </p>
+    <p> {{ phone }}</p>
+    <p> {{ mail}}</p>
     <GenericActions/>
   </div>
 </template>
@@ -11,5 +11,23 @@
 <script>
   export default{
     name:"contactItem",
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      surname: {
+        type: String,
+        default: '',
+      },
+      mail: {
+        type: String,
+        default: '',
+      },
+      phone: {
+        type: String,
+        required: true,
+      }
+    }
   }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="flex h-screen w-screen items-center justify-center overflow-hidden bg-gray-800">
-    <div class="pt-10 max-w-[800px] w-full shadow-lg">
-      <div class="bg-gray-700 text-gray-400 text-xs font-bold p-4 flex rounded-[10px_10px_0_0]">
-        <div class="w-full">
-          FOTO
-        </div>
-        <div class="w-full">
-          NOMBRE
-        </div>
-        <div class="w-full">
-          TELEFONO
-        </div>
-        <div class="w-full">
-          CORREO
-        </div>
-        <div class="w-full">
-          ACCIONES
-        </div>
+    <div class="pt-10 max-w-[800px] w-full shadow-lg flex flex-col gap-5">
+      <div class="w-full flex justify-end">
+        <NuxtLink to="add-contact" class="flex items-center justify-center h-10 w-fit flex gap-3 px-3 rounded bg-gray-700 hover:bg-blue-500 text-white shadow-lg">
+          <NuxtImg src="plususer.svg" class="w-4" />
+          Agregar contacto
+        </NuxtLink>
       </div>
-      <div class="overflow-y-scroll scrollbar max-h-[500px] min-h-[500px] bg-gray-700 rounded-[0_0_10px_10px]">
-        <GenericContactItem />
+      <div>
+        <div class="bg-gray-700 text-gray-400 text-xs font-bold p-4 grid grid-cols-[100px_1fr_1fr_1fr_100px] rounded-[10px_10px_0_0]">
+          <div class="w-[100px]">
+            FOTO
+          </div>
+          <div class="w-full">
+            NOMBRE
+          </div>
+          <div class="w-full">
+            TELEFONO
+          </div>
+          <div class="w-full">
+            CORREO
+          </div>
+          <div class="w-full">
+            ACCIONES
+          </div>
+        </div>
+        <div class="overflow-y-scroll scrollbar max-h-[500px] min-h-[500px] bg-gray-700 rounded-[0_0_10px_10px]">
+          <GenericContactItem
+            name="David"
+            surname="Broncano"
+            phone="644 03 30 22"
+            mail="davidbroncano@gmail.com"
+          />
+        </div>
       </div>
     </div>
   </div>
